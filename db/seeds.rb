f2464a3b5b89555a6902ad9a549bb1243c6c3e39
Irwanto Wibowo <42676690+rinapratama335@users.noreply.github.com>
2020-04-16 00:16:02 +0700

    membuat seeder untuk mengisi tabel secara otomatis (#16)
    Co-authored-by: irwantowibowo1992 <irwantowibowo1992@hotmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Author.create(name: "Irwanto Wibowo", address: 'Gunungkidul, Yogyakarta', old: 28)
+Author.create(name: "Rina Pratama", address: 'Gianyar, Bali', old: 25)
+Author.create(name: "Andi Kurniawan", address: 'Pacitan, Jawa Timur', old: 23)
+Author.create(name: "Wahyu Iswanto", address: 'Magelang, Jawa Tengah', old: 29)
+Author.create(name: "Nurul Kharimah", address: 'Bandung, Jawa Barat', old: 22)
