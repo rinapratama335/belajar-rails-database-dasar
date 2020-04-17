bd12a7ca6576d9cb6499fed6465831e463983229
irwantowibowo1992 <irwantowibowo1992@hotmail.com>
2020-04-18 01:44:51 +0700

    membuat validasi length

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ApplicationRecord
   # belongs_to :author
 
   validates :title, presence: true
-  validates :description, presence: true
+  validates :description, presence: true, length: {minimum: 10, maximum: 20}
 
   def self.expensive
     where('price > 200000')
