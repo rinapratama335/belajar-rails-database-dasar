b72bf83749ed6c60a4dfaa535bc5e6111ecfa40c
irwantowibowo1992 <irwantowibowo1992@hotmail.com>
2020-04-19 00:29:46 +0700

    menyimpan data user yang login dengan session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+
+  def current_user
+    if session[:user_id]
+      User.find(session[:user_id])
+    else
+      nil
+    end
+  end
+
+  def user_signed_in?
+    if current_user
+      true
+    else
+      redirect_to new_session_path, notice: 'Silahkan login terlebih dahulu'
+      return false
+    end
+  end
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_action :user_signed_in?
+
   # layout :false
   layout 'admin'
 
