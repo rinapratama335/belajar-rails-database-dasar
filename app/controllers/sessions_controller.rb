312690e603c993104821ba5d2ebf52a3f6d53de2
Irwanto Wibowo <42676690+rinapratama335@users.noreply.github.com>
2020-04-19 00:03:56 +0700

    membuat halaman web untu login user (#48)
    Co-authored-by: irwantowibowo1992 <irwantowibowo1992@hotmail.com>

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    # render plain: params.inspect
+
+    username = params[:username]
+    password = params[:password]
+
+    user = User.find_by(username: username)
+    if user
+      if user.authenticate(password)
+        redirect_to books_path, notice: 'Kamu berhasil login'
+      else
+        redirect_to new_session_path, notice: 'Username / Password Salah'
+      end
+    else
+      redirect_to new_session_path, notice: 'Username / Password Salah'
+    end
+  end
+end
